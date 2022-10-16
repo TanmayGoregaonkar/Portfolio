691cab0dd9f913433c0e8e8e4b8c3d66c7acf6ea
TanmayGoregaonkar <93464019+TanmayGoregaonkar@users.noreply.github.com>
2022-10-16 17:09:59 +0530

    initiL

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,11 +4,13 @@ import {FaGithubSquare} from 'react-icons/fa'
 import {SiCodechef} from 'react-icons/si'
 const HeaderSocials = () => {
   return (
+
     <div className='header__socials'>
         <a href="https://www.linkedin.com/in/tanmay-goregaonkar-99366021b" target="_blank"><BsLinkedin/></a>
         <a href="https://github.com/TanmayGoregaonkar" target="_blank"><FaGithubSquare/></a>
         <a href="https://www.codechef.com/users/tanmayg790"><SiCodechef/></a>
     </div>
+    
   )
 }
 
